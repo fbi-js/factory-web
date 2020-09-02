123b0f53999f83ba590d67bba03eac81e67e5b73
罗松林 <wx_583f56451d2f42c198762aa113571e73@git.code.tencent.com>
2020-09-02 14:52:37 +0800

    feat: 新增vue3+tsx的模版

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,10 +3,11 @@ import CommandBuild from './commands/build';
 import CommandServe from './commands/serve';
 import TemplateVue from './templates/vue';
 import TemplateReact from './templates/react';
+import TemplateVue3 from './templates/vue3';
 export default class FactoryWeb extends Factory {
     id: string;
     description: string;
     commands: (CommandBuild | CommandServe)[];
-    templates: (TemplateVue | TemplateReact)[];
+    templates: (TemplateVue | TemplateReact | TemplateVue3)[];
     execOpts: any;
 }
