273120357a23780428b31d48c52d449c0d55d2d1
yzstrive <yzstrive@gmail.com>
2020-09-21 19:32:33 +0800

    docs: add standard-version

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -6,7 +6,7 @@ import TemplateMicro from './templates/micro';
 export default class FactoryWeb extends Factory {
     id: string;
     description: string;
-    commands: (CommandBuild | CommandServe)[];
+    commands: (CommandServe | CommandBuild)[];
     templates: (TemplateReact | TemplateMicro)[];
     execOpts: any;
 }
