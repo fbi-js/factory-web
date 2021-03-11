e40192f82c4d14785912d8ea61de7db4f6dc07df
taomasliu <taomasliu@tencent.com>
2021-03-11 20:09:35 +0800

    feat: fix template error

--- templates/vue/webpack.config.js
+++ templates/vue/webpack.config.js
@@ -1,5 +1,5 @@
 // Docs: https://github.com/fbi-js/factory-web#Configuration
-const config = require('@fbi-js/webpack-config-react').default
+const config = require('@fbi-js/webpack-config-vue').default
 
 module.exports = config({
   options: {
